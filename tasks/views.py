0610a508d0f8cc6390f94fc60aa83d67d7468955
Shray Tyagi <shraytyagi1408@gmail.com>
2020-06-27 17:31:50 +0530

    first commit

--- tasks/views.py
+++ tasks/views.py
@@ -16,3 +16,24 @@ def index(request):
     return render(request, 'list.html', context)
 
 
+def updateTask(request, pk):
+    task=Task.objects.get(id=pk)
+    form=TaskForm(instance=task)
+
+    if request.method=='POST':
+        form =TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    context={'form': form}
+    return render(request, 'update_task.html', context)
+
+
+def deleteTask(request, pk):
+    item=Task.objects.get(id=pk)
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+
+    context={'item': item}
+    return render(request, 'delete.html', context)
